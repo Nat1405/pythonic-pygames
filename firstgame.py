ef9dfc6541afc0337321ab102264d169779fbf4b
Nat1405 <nathanc1945@gmail.com>
2017-01-29 13:31:01 -0800

    Update repo with a readme and the runtime game engine loop.

--- firstgame.py
+++ firstgame.py
@@ -16,3 +16,36 @@ PI = 3.141592653
 size = (700, 500)
 screen = pygame.display.set mode(size)
 
+# Set the window title
+
+pygame.display.set caption("My Super Mega Awesome Pygame")
+
+# Now let us set up the main program loop
+
+# Loop until the user clicks the close button
+
+done = False
+
+# This is used to manage how fast the screen updates
+
+clock = pygame.time.Clock()
+
+# ------------ Main Program Loop -----------
+while not done:
+	# ---- Main event loop
+	for event in pygame.event.get(): # User did something
+		if event.type == pygame.QUIT: # If user clicked close
+			done = True # Flag as done so we exit this loop
+	# --- Game logic should go here
+
+	# Drawing code should go here
+	
+	# First, clear the screen to go white. Don't put other drawing
+	# commmands above this, or they will be erased with this command.
+	screen.fill(WHITE)
+
+	# --- Go ahead and update the screen with what we've drawn.
+	pygame.display.flip()
+
+	# --- Limit to 60 frames per second.
+	clock.tick(60)
