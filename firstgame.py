00b320be348bdc474d6775388c265befb68f98d7
Nat1405 <nathanc1945@gmail.com>
2017-01-28 21:53:37 -0800

    First commit

---
+++ firstgame.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+#Define some colors to use in our game
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED =   ( 255, 0, 0)
+BLUE =  (0, 0, 255)
+#Define some constants that we will need
+PI = 3.141592653
+
+# Open a new window and set the window size
+
+size = (700, 500)
+screen = pygame.display.set mode(size)
+
